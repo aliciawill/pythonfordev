464760552b09bd5853caa0da69190c2d8be4f496
Daeseob Lee <daeseob.lee@gmail.com>
2019-02-06 18:48:37 +0900

    initial commit

---
+++ ch15/search_server.py
@@ -0,0 +1,28 @@
+# 앞서 만든 영화 정보를 모으는 함수를 임포트합니다
+from movie_search import collect_movie_info
+from flask import Flask
+from flask import request
+from flask import render_template
+import json
+
+app = Flask(__name__)
+
+app.debug = True
+
+# 결과를 json으로 저장합니다.
+@app.route("/movie-search")
+def search():
+    q = request.args.get("q")
+    return json.dumps(collect_movie_info(q),ensure_ascii=False).encode('utf8')
+
+# 결과를 html 페이지로 보여줍니다.
+@app.route("/movie-search-pretty")
+def search_pretty():
+    q = request.args.get("q")
+    result = collect_movie_info(q)
+    return render_template('result.html',item = result)
+
+# http://127.0.0.1:5000/movie-search?q=알리타
+# http://127.0.0.1:5000/movie-search-pretty?q=알리타
+if __name__ == "__main__":
+    app.run()
